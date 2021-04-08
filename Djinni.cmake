f0483785c5e20415901fefb13d007382945e3f5d
Johannes Krafft <JohannesKrafft@web.de>
2021-04-08 18:03:03 +0200

    initial commit: first attemt for a simple djinni cmake wrapper

---
+++ Djinni.cmake
@@ -0,0 +1,180 @@
+# MIT License
+#
+# Djinni CMake
+# https://github.com/jothepro/djinni-cmake
+#
+# Copyright (c) 2021 jothepro
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+cmake_minimum_required(VERSION 3.18)
+
+function(djinni LIBRARY_TARGET)
+    cmake_parse_arguments(DJINNI
+        # options
+            ""
+        # one-value keywords
+            "IDL;NAMESPACE;DIRECTORY;JAR_OUTPUT_DIR"
+        # multi-value keywords
+            ""
+        # args
+            ${ARGN}
+    )
+
+    find_package(djinni-support-lib)
+
+    set(MESSAGE_PREFIX "[djinni]")
+
+    if(NOT DEFINED DJINNI_DIRECTORY)
+        set(DJINNI_DIRECTORY djinni-generated)
+    endif()
+
+    if(NOT DEFINED DJINNI_NAMESPACE)
+        set(DJINNI_NAMESPACE Djinni)
+    endif()
+
+    if(NOT DEFINED DJINNI_JAR_OUTPUT_DIR)
+        set(DJINNI_JAR_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
+    endif()
+
+    # derive namespaces, prefixes, paths from NAMESPACE
+    string(REPLACE "::" "/" DJINNI_NAMESPACE_PATH ${DJINNI_NAMESPACE})
+    set(DJINNI_JNI_NAMESPACE "${DJINNI_NAMESPACE}::jni")
+    set(DJINNI_OBJCPP_NAMESPACE "${DJINNI_NAMESPACE}::objcpp")
+
+
+    string(REPLACE "::" "." DJINNI_JAVA_PACKAGE ${DJINNI_NAMESPACE})
+    string(REPLACE "-" "_" DJINNI_JAVA_PACKAGE ${DJINNI_JAVA_PACKAGE})
+    string(TOLOWER ${DJINNI_JAVA_PACKAGE} DJINNI_JAVA_PACKAGE)
+    string(TOLOWER ${DJINNI_JAVA_PACKAGE} DJINNI_JAVA_PATH)
+    string(REPLACE "." "/" DJINNI_JAVA_PATH ${DJINNI_JAVA_PATH})
+
+    string(REGEX REPLACE "[a-z:\\-_]" "" DJINNI_OBJC_PREFIX ${DJINNI_NAMESPACE})
+
+    # prepare input variables
+    set(DJINNI_INCLUDE_DIR ${DJINNI_DIRECTORY}/include)
+    set(DJINNI_CPP_OUT ${DJINNI_DIRECTORY}/cpp/src)
+    set(DJINNI_CPP_INCLUDE_PREFIX ${DJINNI_NAMESPACE_PATH}/)
+    set(DJINNI_CPP_INCLUDE_DIR ${DJINNI_DIRECTORY}/cpp/include/)
+    set(DJINNI_CPP_HEADER_OUT ${DJINNI_CPP_INCLUDE_DIR}/${DJINNI_CPP_INCLUDE_PREFIX})
+    set(DJINNI_JNI_OUT ${DJINNI_DIRECTORY}/jni/src)
+    set(DJINNI_JNI_INCLUDE_PREFIX ${DJINNI_NAMESPACE_PATH}/jni/)
+    set(DJINNI_JNI_INCLUDE_DIR ${DJINNI_DIRECTORY}/jni/include/)
+    set(DJINNI_JNI_HEADER_OUT ${DJINNI_JNI_INCLUDE_DIR}/${DJINNI_JNI_INCLUDE_PREFIX})
+    set(DJINNI_JNI_INCLUDE_CPP_PREFIX ${DJINNI_NAMESPACE_PATH}/)
+    set(DJINNI_OBJC_OUT ${DJINNI_DIRECTORY}/objc/src)
+    set(DJINNI_OBJC_INCLUDE_PREFIX ${DJINNI_NAMESPACE_PATH}/objc/)
+    set(DJINNI_OBJCPP_INCLUDE_CPP_PREFIX ${DJINNI_NAMESPACE_PATH}/)
+    set(DJINNI_OBJC_INCLUDE_DIR ${DJINNI_DIRECTORY}/objc/include/)
+    set(DJINNI_OBJC_HEADER_OUT ${DJINNI_OBJC_INCLUDE_DIR}/${DJINNI_OBJC_INCLUDE_PREFIX})
+    set(DJINNI_OBJCPP_OUT ${DJINNI_DIRECTORY}/objcpp/src/)
+    set(DJINNI_JAVA_OUT ${DJINNI_DIRECTORY}/java/${DJINNI_JAVA_PATH})
+    set(DJINNI_OBJC_SWIFT_BRIDGING_HEADER ${LIBRARY_TARGET})
+    set(DJINNI_JAVA_LIBRARY_TARGET ${LIBRARY_TARGET}-android)
+
+    find_program(DJINNI_EXECUTABLE djinni REQUIRED)
+
+    # output djinni version
+    execute_process(COMMAND ${DJINNI_EXECUTABLE} --version
+        OUTPUT_VARIABLE DJINNI_VERSION_OUTPUT)
+    message(STATUS "${MESSAGE_PREFIX} ${DJINNI_VERSION_OUTPUT}")
+
+    set(DARWIN_OS_LIST "Darwin;iOS;tvOS;watchOS")
+    if(CMAKE_SYSTEM_NAME IN_LIST DARWIN_OS_LIST)
+        set(DARWIN 1)
+    endif()
+    if(ANDROID)
+        find_package(Java 1.8 REQUIRED)
+        include(UseJava)
+
+        set(DJINNI_GENERATED_JAVA_FILES_OUTFILE ${CMAKE_CURRENT_BINARY_DIR}/djinni-generated-java-files.txt)
+
+        message(STATUS "${MESSAGE_PREFIX} Generating Java Gluecode")
+        # generate java code
+        execute_process(COMMAND ${DJINNI_EXECUTABLE}
+                --idl ${DJINNI_IDL}
+                --java-out ${DJINNI_JAVA_OUT}
+                --java-package ${DJINNI_JAVA_PACKAGE}
+                --list-out-files ${DJINNI_GENERATED_JAVA_FILES_OUTFILE}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                )
+
+        file(STRINGS ${DJINNI_GENERATED_JAVA_FILES_OUTFILE} DJINNI_GENERATED_JAVA_FILES
+                ENCODING UTF-8)
+
+        add_jar(${DJINNI_JAVA_LIBRARY_TARGET}
+                SOURCES ${DJINNI_GENERATED_JAVA_FILES}
+                OUTPUT_DIR ${DJINNI_JAR_OUTPUT_DIR}
+                OUTPUT_NAME ${LIBRARY_TARGET})
+
+        set(ADDITIONAL_DJINNI_PARAMETERS
+                --jni-out ${DJINNI_JNI_OUT}
+                --jni-header-out ${DJINNI_JNI_HEADER_OUT}
+                --jni-namespace ${DJINNI_JNI_NAMESPACE}
+                --jni-include-prefix ${DJINNI_JNI_INCLUDE_PREFIX}
+                --jni-include-cpp-prefix ${DJINNI_JNI_INCLUDE_CPP_PREFIX})
+
+    elseif(DARWIN)
+        set(ADDITIONAL_DJINNI_PARAMETERS
+                --objc-out ${DJINNI_OBJC_OUT}
+                --objc-header-out ${DJINNI_OBJC_HEADER_OUT}
+                --objc-type-prefix ${DJINNI_OBJC_PREFIX}
+                --objcpp-out ${DJINNI_OBJCPP_OUT}
+                --objcpp-namespace ${DJINNI_OBJCPP_NAMESPACE}
+                --objc-include-prefix ${DJINNI_OBJC_INCLUDE_PREFIX}
+                --objcpp-include-cpp-prefix ${DJINNI_OBJCPP_INCLUDE_CPP_PREFIX}
+                --objcpp-include-objc-prefix ${DJINNI_OBJC_INCLUDE_PREFIX}
+                --objc-swift-bridging-header ${DJINNI_OBJC_SWIFT_BRIDGING_HEADER})
+    endif()
+
+    set(DJINNI_GENERATED_FILES_OUTFILE ${CMAKE_CURRENT_BINARY_DIR}/djinni-generated-files.txt)
+
+    message(STATUS "${MESSAGE_PREFIX} Generating C++ Gluecode")
+    # generate c++ interface
+    execute_process(COMMAND ${DJINNI_EXECUTABLE}
+            --idl ${DJINNI_IDL}
+            --cpp-out ${DJINNI_CPP_OUT}
+            --cpp-namespace ${DJINNI_NAMESPACE}
+            --cpp-header-out ${DJINNI_CPP_HEADER_OUT}
+            --cpp-include-prefix ${DJINNI_CPP_INCLUDE_PREFIX}
+            --list-out-files ${DJINNI_GENERATED_FILES_OUTFILE}
+            ${ADDITIONAL_DJINNI_PARAMETERS}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+
+    file(STRINGS ${DJINNI_GENERATED_FILES_OUTFILE} DJINNI_GENERATED_CPP_FILES
+        ENCODING UTF-8)
+
+    add_library(${LIBRARY_TARGET} ${DJINNI_GENERATED_CPP_FILES})
+
+    target_compile_features(${LIBRARY_TARGET} PUBLIC cxx_std_17)
+
+    target_include_directories(${LIBRARY_TARGET} PUBLIC ${DJINNI_CPP_INCLUDE_DIR})
+
+    target_link_libraries(${LIBRARY_TARGET} PUBLIC djinni-support-lib::djinni-support-lib)
+
+    if(ANDROID)
+        add_dependencies(${LIBRARY_TARGET} ${DJINNI_JAVA_LIBRARY_TARGET})
+        target_include_directories(${LIBRARY_TARGET} PUBLIC ${DJINNI_JNI_INCLUDE_DIR})
+    elseif(DARWIN)
+        target_compile_options(${LIBRARY_TARGET} PRIVATE -fobjc-arc)
+        target_include_directories(${LIBRARY_TARGET} PUBLIC ${DJINNI_OBJC_INCLUDE_DIR})
+    endif()
+
+endfunction()
--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# djinni-cmake
-CMake wrapper for djinni
+# Djinni CMake 🧞‍♂️
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/jothepro/djinni-cmake)](https://github.com/jothepro/djinni-cmake/releases/latest)
+[![GitHub](https://img.shields.io/github/license/jothepro/djinni-cmake)](https://github.com/jothepro/djinni-cmake/blob/main/LICENSE)
+
+Simple CMake wrapper for [Djinni](https://djinni.xlcpp.dev/).
+
+## Motivation
+
+While I like to have a good portion of configuration options in the [Djinni Generator](https://github.com/cross-language-cpp/djinni-generator), 
+I think it is easier to get started with Djinni if a few presumptions are made for the developer.
+
+This wrapper attemts to be a tool that allows a quick and simple start into a new mobile project with C++ using Djinni.
+
+It may evolve over time to a more powerful tool with more configuration options. This depends on your feedback and my future requirements.
+
+## Features
+
+- 🎯 Easy to use
+- 🧶 Little configuration required
+- 🧩 Convention over configuration
+
+## Prerequisites
+
+- [CMake](https://cmake.org/) >= 3.18
+- [Djinni Generator](https://github.com/cross-language-cpp/djinni-generator) >= 0.3.2
+- [Djinni Support Lib](https://github.com/cross-language-cpp/djinni-support-lib) >= 0.0.1 (must be available as CMake target `djinni-support-lib::djinni-support-lib`)
+
+## Installation
+
+Copy the file `Djinni.cmake` from the latest release to your cmake modules folder and include it in the root `CMakeLists.txt`:
+
+```cmake
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules ${CMAKE_MODULE_PATH})
+include(Djinni)
+```
+
+Watch this repository so you don't miss updates! 🔔
+
+## Synopsis
+
+```cmake
+djinni(<target> 
+        IDL <filename>
+        [NAMESPACE <namespace>]
+        [DIRECTORY <output-dir>]
+        [JAR_OUTPUT_DIR <jar-output-dir>]
+)
+```
+
+Calls Djinni Generator and creates a target with name `<target>` from the generated sources.
+
+Automatically detects for which platform to configure the generator, depending on `CMAKE_SYSTEM_NAME`.
+If building for Android, additionally a target `<target>-android` is created, that builds a jar named `<target>.jar` with the Java gluecode to `<jar-output-dir>` 
+when the target `<target>` is built.
+
+If a non supported target platform (everything except Android, iOS, macOS, tvOS, watchOS) is detected, only the C++ interface is generated.
+
+This generator is intentionally favouring convention over configuration to keep things as simple as possible.
+If you miss a configuration option anyways, please consider opening an issue.
+
+## Options
+
+The options are:
+
+- `IDL <filename>`<br>
+  filename/path of the Djinni-IDL file that should be processed.
+- `NAMESPACE <namespace>`<br>
+  Optional; Default: `Djinni`<br>
+  The namespace for the generated code. Each namespace part should start with an uppercase letter.
+  The namespace is used for the generated C++ code, but also automatically transformed to a Java package & ObjC prefix.<br>
+  Examples:
+  
+  | `NAMESPACE` value | C++ namespace          | Java package           | ObjC prefix |
+  | ----------------- | ---------------------- | ---------------------- | ------------|
+  | `Djinni::Lib`     | `Djinni::Lib`          | `djinni.lib`           | `DL`        |
+  | `My::LibExample`| `My::LibExample`         | `my.libexample`        | `MLE`       |
+  | `My::Djinni-Lib`  | `My::Djinni-Lib`       | `my.djinni_lib`        | `MDL`       |
+- `DIRECTORY <output-dir>`<br>
+  Optional; Default: `djinni-generated`<br>
+  The output directory where the generated code should be written to.
+- `JAR_OUTPUT_DIR <jar-output-dir>`<br>
+  Optional; Default: `${CMAKE_CURRENT_BINARY_DIR}`<br>
+  The directory to which the jar should be written if gluecode for Android is created.
+
+## Example
+
+*(A full project example is coming soon, stay tuned)*
+
+Given a Djinni-IDL file named `example.djinni`, this is all you need in your `CMakeLists.txt`:
+
+```cmake
+djinni(Example
+    IDL example.djinni
+    NAMESPACE Demo
+)
+```
+
+This will generate a target `Example` that contains all the required gluecode from the interface defined in `example.djinni`.
+
+All C++ classes will be in the namespace `Demo`, all Java classes in the package `demo` and all ObjC structures will have the prefix `D`.
+
+All generated header files can be found on the include path under `Demo/...`
+
+If the target platform is Android, a jar named `Example.jar` will be built to `${CMAKE_CURRENT_BINARY_DIR}` once the target `Example` is built.
+
+If the target platform is Darwin (iOS/macOS), a Swift Bridging Header can be found on the include path: `Demo/ExampleBridge.h`
+
+## Credits
+
+Thanks to the work of @freitass ([Djinni.cmake](https://github.com/cross-language-cpp/djinni-support-lib/blob/main/test-suite/Djinni.cmake)) and @a4z ([djinni_process_idl.cmake](https://github.com/cross-language-cpp/djinni-example-cc/blob/main/cmake/djinni_process_idl.cmake))
+for inspiring me to write this wrapper.
